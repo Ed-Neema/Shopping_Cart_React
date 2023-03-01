53b197f7d704788a574e35cbf7a3762f0431405c
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-03-02 01:41:01 +0200

    check

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -14,7 +14,27 @@ const NavbarComponent = () => {
     setShow(true);
   };
 
-  const productsCount =  cart.items.reduce((sum,product)=> sum + product.quantity,0)
+  const productsCount =  cart.items.reduce((sum,product)=> sum + product.quantity,0);
+
+  // send request to backend when checkout is clicked
+  const checkout = async() => {
+    await fetch("http://localhost:4000/checkout", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({items: cart.items})
+    }).then((response)=>{ //take response from server and convert to json
+      return response.json();
+
+    }).then((response)=>{
+      if(response.url){
+        //if url exists (remember this is where we are sending the user to the session url in server.js),
+        // forward user to stripe
+        window.location.assign(response.url);
+      }
+    });
+  }
   return (
     <div>
       <Navbar expand="sm">
@@ -41,7 +61,7 @@ const NavbarComponent = () => {
                 ></CartProducts>
               ))}
               <h1>Total: {cart.getTotalCost().toFixed(2)} </h1>
-              <Button variant="success">Checkout</Button>
+              <Button variant="success" onClick={checkout}>Checkout</Button>
             </>
           ) : (
             <h1>There are no items in your Cart 🙂</h1>
--- src/productStore.js
+++ src/productStore.js
@@ -1,28 +1,32 @@
+// Coffee: price_1MgySdKi4ZLh3cdghOFpnRGA
+// Sunglasses: price_1MgyU0Ki4ZLh3cdgS13wvovw
+// Camera: price_1MgyUiKi4ZLh3cdg2jwIE7rF
+
 const productsArray = [
-    {
-        id:"1",
-        title:"Coffee",
-        price: 4.99
-    },
-    {
-        id:"2",
-        title:"Sunglasses",
-        price: 9.99
-    },
-    {
-        id:"3",
-        title:"Camera",
-        price: 39.99
-    },
+  {
+    id: "price_1MgySdKi4ZLh3cdghOFpnRGA",
+    title: "Coffee",
+    price: 4.99,
+  },
+  {
+    id: "price_1MgyU0Ki4ZLh3cdgS13wvovw",
+    title: "Sunglasses",
+    price: 9.99,
+  },
+  {
+    id: "price_1MgyUiKi4ZLh3cdg2jwIE7rF",
+    title: "Camera",
+    price: 39.99,
+  },
 ];
 
-function getProductData(id){
-    let  productData = productsArray.find(product => product.id === id);
+function getProductData(id) {
+  let productData = productsArray.find((product) => product.id === id);
 
-    if(productData === undefined){
-        console.log("Product data doesn't exist with ID: " + id);
-        return undefined;
-    }
-    return productData;
+  if (productData === undefined) {
+    console.log("Product data doesn't exist with ID: " + id);
+    return undefined;
+  }
+  return productData;
 }
 export { productsArray, getProductData };
